9dc7b9e2b6e7c6d1d71cb251d8156af787cb38b6
zzfuture <113058239+zzfuture@users.noreply.github.com>
2023-08-26 12:11:26 -0500

    fix: 🐛 fixed incompatibility between main's grid and sidebar

--- js/main.js
+++ js/main.js
@@ -1,17 +1,21 @@
 function sidebarOpen(){
+    const sidebarContainer = document.querySelector("#sidebar-container");
     const sidebar = document.querySelector("#sidebar");
     const background = document.querySelector("#sidebar-background");
     const body = document.querySelector("body");
+    sidebarContainer.style = 'z-index: 300';
     sidebar.style = 'left: 0px';
-    background.style = 'background: rgba(0, 0, 0, 0.400); display: block;';
+    background.style = 'background-color: rgba(0, 0, 0, 0.400); z-index: 400;';
     body.style = 'overflow-y: hidden;'
 }
 function sidebarClose(){
+    const sidebarContainer = document.querySelector("#sidebar-container");
     const sidebar = document.querySelector("#sidebar");
     const background = document.querySelector("#sidebar-background");
     const body = document.querySelector("body");
+    sidebarContainer.style = 'z-index: -1';
     sidebar.style = 'left: -400px';
-    background.style = 'background: transparent; display: none;';
+    background.style = 'z-index: 0; visibility: hidden';
     body.style = 'overflow-y: visible;'
 }
 
